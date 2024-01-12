4ac39bbc4f0ad8c82562d4799a85e2c8b0719612
Tri Thien Nguyen <anretrithien@gmail.com>
2024-01-12 15:15:55 +0000

    modify more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,39 +5,49 @@ import styles from "@/app/home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Link href="/page1">
-        <div className={styles.imageContainer}>
-          <Image
-            src="/pictures/1.png"
-            alt="Viet80s Restaurant"
-            layout="fill"
-            objectFit="cover"
-          />
-          <p className={styles.imageText}>VIET80S RESTAURANT</p>
-        </div>
-      </Link>
-      <Link href="/page2">
+      <Link href="/street-food">
         <div className={styles.imageContainer}>
           <Image
             src="/pictures/2.png"
             alt="Viet80s Street Food"
-            layout="fill"
-            objectFit="cover"
+            fill="responsive"
+            style={{ objectFit: "cover" }}
+            priority={false}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
-          <p className={styles.imageText}>VIET80S STREET FOOD</p>
+          <p className={styles.imageText}>STREET FOOD</p>
         </div>
       </Link>
-      <Link href="/page3">
+      <Link href="/restaurant">
+        <div className={styles.imageContainer}>
+          <Image
+            src="/pictures/1.png"
+            alt="Viet80s Restaurant"
+            fill="responsive"
+            style={{ objectFit: "cover" }}
+            priority={false}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+          <p className={styles.imageText}>RESTAURANT</p>
+        </div>
+      </Link>
+      <Link href="/coffee">
         <div className={styles.imageContainer}>
           <Image
             src="/pictures/3.png"
             alt="Viet80s Coffee"
-            layout="fill"
-            objectFit="cover"
+            fill="responsive"
+            style={{ objectFit: "cover" }}
+            priority={false}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
-          <p className={styles.imageText}>VIET80S COFFEE</p>
+          <p className={styles.imageText}>COFFEE</p>
         </div>
       </Link>
+      <footer className={styles.footer}>
+        <p>57-67 Friar Lane, Nottingham NG1 6DH</p>
+        <p>&copy; {new Date().getFullYear()} Viet80s LTD</p>
+      </footer>
     </div>
   );
 }
---
+++ src/app/restaurant/page.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/app/home.module.css";
+
+export default function Home() {
+  return <h1>hello</h1>;
+}
