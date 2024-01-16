08509676e598e1503ff9bc622d31e3448f2e0443
Tri Thien Nguyen <anretrithien@gmail.com>
2024-01-16 15:22:49 +0000

    carousel

--- src/app/restaurant/page.tsx
+++ src/app/restaurant/page.tsx
@@ -5,12 +5,14 @@ import { slide as Menu } from "react-burger-menu";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import Sidebar from "@/components/SideBar";
+import AutoPlay from "@/components/Carousel";
+import SimpleSlider from "@/components/Carousel";
 
 export default function Home() {
   return (
     <>
       <Sidebar />
-      <div className="flex flex-col items-center justify-center bg-black">
+      <div className="sticky top-0 z-50 flex flex-col items-center justify-center bg-black">
         <Link href="/" className="mb-5 mt-5">
           <Image
             src="/pictures/logo.png"
@@ -22,7 +24,7 @@ export default function Home() {
         </Link>
       </div>
       <div>
-        <h1>Hello</h1>
+        <AutoPlay />
       </div>
     </>
   );
