e42ff83323230e9f3741d140e1a8abfd1ef9d48d
Tri Thien Nguyen <anretrithien@gmail.com>
2024-01-26 16:02:22 +0000

    links for blog articles and transition home page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -10,17 +10,23 @@ import styles from "@/app/blog.module.css";
 
 export default function Home() {
   const isMobile = useMediaQuery({ maxWidth: 768 });
-
+  const fontSize = isMobile ? "35px" : "45px";
   return (
     <>
       <NavBar />
       <div className="bg-black">
+        <h1
+          style={{
+            textAlign: "center",
+            color: "#F8C983",
+            fontSize: fontSize,
+            marginBottom: "5px",
+          }}
+        >
+          Viet80s Blog
+        </h1>
         <div className={styles.container}>
-          <Link
-            href="/docs/menugiaySF-pdf.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <Link href="/blog/banh-mi">
             <div className={styles.imageContainer}>
               <Image
                 src="/pictures/banhMi.png"
@@ -33,7 +39,7 @@ export default function Home() {
               <p className={styles.imageText}>Bánh Mì</p>
             </div>
           </Link>
-          <Link href="https://www.viet80sonline.co.uk/">
+          <Link href="/blog/pho-viet">
             <div className={styles.imageContainer}>
               <Image
                 src="/pictures/phoViet.png"
@@ -47,11 +53,7 @@ export default function Home() {
             </div>
           </Link>
 
-          <Link
-            href="/docs/Menu_Coffee.png"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <Link href="/blog/ca-phe">
             <div className={styles.imageContainer}>
               <Image
                 src="/pictures/caPhe.png"
@@ -66,11 +68,7 @@ export default function Home() {
           </Link>
         </div>
         <div className={styles.container}>
-          <Link
-            href="/docs/menugiaySF-pdf.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <Link href="/blog/trau-nuoc">
             <div className={styles.imageContainer}>
               <Image
                 src="/pictures/trauNuoc.png"
@@ -83,11 +81,7 @@ export default function Home() {
               <p className={styles.imageText}>Con Trâu Nước</p>
             </div>
           </Link>
-          <Link
-            href="/docs/Menu_Coffee.png"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <Link href="/blog/trong-dong">
             <div className={styles.imageContainer}>
               <Image
                 src="/pictures/trongDong.png"
@@ -100,7 +94,7 @@ export default function Home() {
               <p className={styles.imageText}>Trống Đồng Ngọc Lũ</p>
             </div>
           </Link>
-          <Link href="https://www.viet80sonline.co.uk/">
+          <Link href="/blog/non-la">
             <div className={styles.imageContainer}>
               <Image
                 src="/pictures/nonLa.png"
---
+++ src/app/blog/pho-viet/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useMediaQuery } from "react-responsive";
+import AutoPlayMobile from "@/components/AutoPlayMobile";
+import AutoPlayLaptop from "@/components/AutoPlayLaptop";
+import Footer from "@/components/Footer";
+import styles from "@/app/localHome.module.css";
+import DetailsFooter from "@/components/DetailsFooter";
+import NavBar from "@/components/NavBar";
+import FoodGalleryResto from "@/components/FoodGalleryResto";
+
+export default function Home() {
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
+  return (
+    <>
+      <NavBar />
+      <div className={"bg-black"}>
+        <div>
+          {isMobile ? (
+            <AutoPlayMobile location="restaurant" />
+          ) : (
+            <AutoPlayLaptop location="restaurant" />
+          )}
+        </div>
+        <div className={styles.paragraph}>
+          <p>
+            Welcome to Viet80s, a proud representation of Vietnam’s rich
+            culinary culture and the ultimate Vietnamese dining experience in
+            the heart of Nottingham.
+          </p>
+
+          <p>
+            At Viet80s, we believe that food is not just about nourishment but
+            also about connecting people and cultures. Come and experience the
+            real feeling of Vietnam with authentic Pho and the flavours of
+            Vietnamese traditional recipes today.
+          </p>
+        </div>
+
+        <div className="mr-10 ml-10 mt-10 bg-black">
+          <FoodGalleryResto />
+        </div>
+      </div>
+      <DetailsFooter location="restaurant" />
+      <Footer />
+    </>
+  );
+}
