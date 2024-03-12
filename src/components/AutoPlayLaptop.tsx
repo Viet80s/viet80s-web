a901da6010db171a927719ff66389a68f172d842
Viet80s <137057864+Viet80s@users.noreply.github.com>
2024-03-12 13:05:19 +0000

    test

--- src/components/AutoPlayLaptop.tsx
+++ src/components/AutoPlayLaptop.tsx
@@ -24,15 +24,7 @@ const AutoPlayLaptop = ({ location }: Props) => {
       {location === "restaurant" && (
         <>
           <Slider {...settings}>
-            <div>
-              <Image
-                src="/pictures/promote2.png"
-                alt="Viet80s promote"
-                width={1920}
-                height={780}
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 70vw"
-              />
-            </div>
+           
             <div>
               <Image
                 src="/pictures/4.png"
