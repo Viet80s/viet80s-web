fc5383c500798abe740b889a188d80d37392bfc4
Tri Thien Nguyen <anretrithien@gmail.com>
2024-09-07 15:38:17 +0100

    test again123

--- src/app/quiz/layout.tsx
+++ src/app/quiz/layout.tsx
@@ -4,30 +4,30 @@ import { Toaster } from "react-hot-toast";
 
 const baumans = EB_Garamond({ subsets: ["latin"], weight: ["400"] });
 
-export const metadata: Metadata = {
-  title: "What Vietnamese cuisines are for you | Viet80s",
-  description:
-    "Take this 2-minute quiz to find your inner Vietnamese spirit and what dishes you would absolutely love!",
-  keywords: [
-    "zine",
-    "blog",
-    "blog about Vietnam",
-    "Vietnam",
-    "Vietnamese",
-    "Vietnamese culture",
-    "Vietnamese food",
-    "Vietnamese history",
-    "Vietnamese people",
-    "Vietnamese travel",
-    "Vietnamese zine",
-    "Vietnamese zine about Vietnam",
-  ],
-  openGraph: {
-    title: "Viet80s-quiz",
-    images: ["/pictures/quiz/quizLayout.webp"],
-    siteName: "Viet80s",
-  },
-};
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "What Vietnamese cuisines are for you | Viet80s",
+    description:
+      "Take this 2-minute quiz to find your inner Vietnamese spirit and what dishes you would absolutely love!",
+    keywords: [
+      "zine",
+      "blog",
+      "blog about Vietnam",
+      "Vietnam",
+      "Vietnamese",
+      "Vietnamese culture",
+      "Vietnamese food",
+      "Vietnamese history",
+      "Vietnamese people",
+      "Vietnamese travel",
+      "Vietnamese zine",
+      "Vietnamese zine about Vietnam",
+    ],
+    openGraph: {
+      images: ["/pictures/quiz/quizLayout.webp"],
+    },
+  };
+}
 
 export default function RootLayout({
   children,
